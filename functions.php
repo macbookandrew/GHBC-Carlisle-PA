f0d7d499e94cf306b1968e3b34cda6e494e2b3ea
Andrew Minion <andrew@andrewrminion.com>
2015-02-28 16:18:11 -0500

    switch to SCSS, use minified styles

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
+function add_custom_css() {
+    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/css/style.min.css' );
+    wp_dequeue_style( 'twentyfourteen-style' );
+}
+add_action( 'wp_enqueue_scripts', 'add_custom_css' );
 
 include('functions-branding.php');
 include('functions-custom_post_types.php');
-
-?>
