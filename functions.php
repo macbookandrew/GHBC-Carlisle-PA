1b55202adb0bba4948d39bfe049ed3d54210f92c
Andrew Minion <andrew@andrewrminion.com>
2015-03-14 21:06:40 -0400

    remove 2013 fonts, add Alegreya webfont

--- functions.php
+++ functions.php
@@ -2,8 +2,16 @@
 function add_custom_css() {
     wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/style.min.css' );
     wp_dequeue_style( 'twentythirteen-style' );
+
+    // fonts
+    wp_dequeue_style( 'twentythirteen-fonts' );
+    wp_deregister_style( 'open-sans' );
+    wp_register_style( 'open-sans', false ); // dependency workaround from https://gist.github.com/thetrickster/8946567
+    wp_dequeue_style( 'bitter' );
+
+    wp_enqueue_style( 'webfonts', '//fonts.googleapis.com/css?family=Alegreya+Sans:400,700|Alegreya:400,700' );
 }
-add_action( 'wp_enqueue_scripts', 'add_custom_css' );
+add_action( 'wp_print_styles', 'add_custom_css' );
 
 include('functions-branding.php');
 include('functions-church.php');
