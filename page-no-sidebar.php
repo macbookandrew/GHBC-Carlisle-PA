850ab1794802b2195357c44fad0e64451c013c54
Andrew Minion <andrew@andrewrminion.com>
2015-03-31 10:46:21 -0400

    tweak to full-width page template (no sidebar)

--- page-no-sidebar.php
+++ page-no-sidebar.php
@@ -1,15 +1,9 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
+
+Template Name: Full Width (no sidebar)
+
+*/
 
 get_header(); ?>
 
@@ -46,5 +40,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
